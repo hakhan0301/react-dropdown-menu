120f0019581542aeaa1a5107a7726c18a972b7c0
Hassan <hakhan0301@gmail.com>
2022-10-27 19:57:45 -0500

    feat: dynamic multi-select

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ function App() {
     <div className="bg-red-500 min-h-screen">
       <div className="flex justify-center p-12">
         <div className="flex flex-col gap-5">
-          <DropdownMenu />
-          <DropdownMenu />
+          <DropdownMenu options={["a", 'b']} />
+          <DropdownMenu options={["apple", 'bbanana', 'fasd', 'fads', 'fadsfasdf', 'fsadfsd']} multiselect={true} />
         </div>
       </div>
     </div>
--- src/components/DropdownMenu.tsx
+++ src/components/DropdownMenu.tsx
@@ -1,23 +1,73 @@
+
+import type { Key, PropsWithoutRef } from 'react';
+import { useMemo } from 'react';
 import { useState } from "react";
 
-function DropdownItem() {
+
+type DropdownItemProps = {
+  text: String,
+  multiselect?: boolean,
+  selected?: boolean,
+  onSelect: () => void
+}
+function DropdownItem({ text, onSelect, multiselect, selected }: PropsWithoutRef<DropdownItemProps>) {
   return (
     <button className={`w-full bg-white px-4 py-2
-            first:rounded-t-lg last:rounded-b-lg
-            hover:cursor-pointer hover:bg-gray-200
-            text-left`}>
+        flex gap-2 items-center
+        first:rounded-t-lg last:rounded-b-lg
+        hover:cursor-pointer hover:bg-gray-200
+        text-left`}
+      onMouseDown={onSelect}
+    >
+      {
+        multiselect && <div className='rounded-sm w-4 h-4 border-2 text-xs text-center'>
+          {selected && "x"}
+        </div>
+      }
       <div className="">
-        test
+        {text}
       </div>
     </button>
   )
 }
 
-function DropdownMenu() {
+export type DropdownMenuProps = {
+  options: String[],
+  multiselect?: boolean
+}
+
+
+const DropdownMenu = ({ options, multiselect }: PropsWithoutRef<DropdownMenuProps>) => {
   const [open, setOpen] = useState<boolean>(false);
 
-  const border = open ? 'border-2 border-blue-500' : 'border-2 border-slate-200';
+  const selectedIndices = useMemo(() => new Set<number>(), []);
+  const selectedOptions = useMemo(
+    () => Array.from(selectedIndices).map(index => options[index])
+    , [selectedIndices.entries(), options]
+  );
 
+
+  const onOptionSelected = (index: number) => {
+    if (selectedIndices.has(index)) {
+      selectedIndices.delete(index);
+    } else {
+      if (!multiselect) {
+        selectedIndices.clear();
+      }
+      selectedIndices.add(index);
+    }
+  }
+
+  const dropdownItems = useMemo(() => options.map(
+    (option, index) => <DropdownItem text={option}
+      selected={selectedIndices.has(index)}
+      multiselect={multiselect}
+      key={option as Key}
+      onSelect={() => onOptionSelected(index)}
+    />
+  ), [options, selectedIndices.entries()]);
+
+  const border = open ? 'border-2 border-blue-500' : 'border-2 border-slate-200';
   return (
     <div className="w-96">
       <button className={`w-full h-12 px-4 rounded-lg
@@ -25,18 +75,16 @@ function DropdownMenu() {
               hover:cursor-pointer`}
         onBlur={() => setOpen(false)}
         onClick={() => setOpen(!open)}>
-        <div className="flex h-full items-center">
+        <div className="flex h-full items-center gap-2">
           <div className="w-full">
-            {open ? 'open' : 'closed'}
+            {selectedOptions.join(", ")}
           </div>
 
           <h1 className="">{open ? '^' : 'v'}</h1>
         </div>
       </button>
       <div className={`w-96 flex flex-col absolute ${open ? '' : 'hidden'}`} role="list">
-        <DropdownItem />
-        <DropdownItem />
-        <DropdownItem />
+        {dropdownItems}
       </div>
     </div>
   );

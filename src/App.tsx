5f22618ff0dcd6e382e057b152b6eac9bbf3bcb4
Hassan <hakhan0301@gmail.com>
2022-10-27 20:24:56 -0500

    feat: pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import DropdownMenu from "./components/DropdownMenu";
 
+const longOptions = new Array(5000)
+  .fill(0)
+  .map((_, index) => `${index}`);
+
 function App() {
   return (
     <div className="bg-red-500 min-h-screen">
       <div className="flex justify-center p-12">
         <div className="flex flex-col gap-5">
           <DropdownMenu options={["a", 'b']} />
-          <DropdownMenu options={["apple", 'bbanana', 'fasd', 'fads', 'fadsfasdf', 'fsadfsd']} multiselect={true} />
+          <DropdownMenu options={longOptions} multiselect={true} />
         </div>
       </div>
     </div>
--- src/components/DropdownMenu.tsx
+++ src/components/DropdownMenu.tsx
@@ -1,5 +1,6 @@
 
-import type { Key, PropsWithoutRef } from 'react';
+import { Key, PropsWithoutRef, useRef } from 'react';
+import { useEffect } from 'react';
 import { useMemo } from 'react';
 import { useState } from "react";
 
@@ -14,16 +15,13 @@ function DropdownItem({ text, onSelect, multiselect, selected }: PropsWithoutRef
   return (
     <button className={`w-full bg-white px-4 py-2
         flex gap-2 items-center
-        first:rounded-t-lg last:rounded-b-lg
         hover:cursor-pointer hover:bg-gray-200
         text-left`}
       onMouseDown={onSelect}
     >
-      {
-        multiselect && <div className='rounded-sm w-4 h-4 border-2 text-xs text-center'>
-          {selected && "x"}
-        </div>
-      }
+      {multiselect && <div className='rounded-sm w-4 h-4 border-2 text-xs text-center'>
+        {selected && "✔️"}
+      </div>}
       <div className="">
         {text}
       </div>
@@ -36,15 +34,38 @@ export type DropdownMenuProps = {
   multiselect?: boolean
 }
 
-
+const ITEMS_PER_PAGE = 20;
 const DropdownMenu = ({ options, multiselect }: PropsWithoutRef<DropdownMenuProps>) => {
   const [open, setOpen] = useState<boolean>(false);
+
+  const [currentPage, setCurrentPage] = useState<number>(0);
+  const slicedOptions = useMemo(
+    () => options.slice(0, (currentPage + 1) * ITEMS_PER_PAGE),
+    [currentPage, options]
+  );
 
   const selectedIndices = useMemo(() => new Set<number>(), []);
   const selectedOptions = useMemo(
     () => Array.from(selectedIndices).map(index => options[index])
     , [selectedIndices.entries(), options]
   );
+
+  const [showClearButton, setShowClearButton] = useState<boolean>(false);
+  useEffect(() => setShowClearButton(selectedIndices.size > 0), [selectedIndices.size]);
+
+  const itemsListRef = useRef();
+  const onDropdownScroll = () => {
+    // referenced from https://blog.saeloun.com/2022/07/08/react-custom-infinite-scroll-with-pagination.html
+    if (itemsListRef.current) {
+      const { scrollTop, scrollHeight, clientHeight } = itemsListRef.current;
+
+      if (scrollTop + clientHeight === scrollHeight) {
+        setCurrentPage(currentPage + 1);
+      }
+    }
+  }
+
+  // console.log(currentPage);
 
 
   const onOptionSelected = (index: number) => {
@@ -58,18 +79,19 @@ const DropdownMenu = ({ options, multiselect }: PropsWithoutRef<DropdownMenuProp
     }
   }
 
-  const dropdownItems = useMemo(() => options.map(
+  const dropdownItems = useMemo(() => slicedOptions.map(
     (option, index) => <DropdownItem text={option}
       selected={selectedIndices.has(index)}
       multiselect={multiselect}
       key={option as Key}
       onSelect={() => onOptionSelected(index)}
     />
-  ), [options, selectedIndices.entries()]);
+  ), [slicedOptions, selectedIndices.entries()]);
 
   const border = open ? 'border-2 border-blue-500' : 'border-2 border-slate-200';
   return (
-    <div className="w-96">
+    <div className="w-96 relative">
+      {/* menu */}
       <button className={`w-full h-12 px-4 rounded-lg
               bg-white  ${border} shadow text-left
               hover:cursor-pointer`}
@@ -79,11 +101,22 @@ const DropdownMenu = ({ options, multiselect }: PropsWithoutRef<DropdownMenuProp
           <div className="w-full">
             {selectedOptions.join(", ")}
           </div>
-
           <h1 className="">{open ? '^' : 'v'}</h1>
         </div>
       </button>
-      <div className={`w-96 flex flex-col absolute ${open ? '' : 'hidden'}`} role="list">
+
+      {/* clear button */}
+      {showClearButton && <button className='absolute right-8 top-[25%]'
+        onClick={() => { selectedIndices.clear(); setShowClearButton(false); }}>
+        x
+      </button>}
+
+      {/* items list */}
+      <div className={`w-96 flex flex-col absolute max-h-60 rounded-l-lg overflow-y-auto z-10 ${open ? '' : 'hidden'}`}
+        role="list"
+        onScroll={onDropdownScroll}
+        ref={itemsListRef as any}
+      >
         {dropdownItems}
       </div>
     </div>
